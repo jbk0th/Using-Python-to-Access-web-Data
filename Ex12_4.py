a62b5d549ada47410d2ffb1ab31af8fa4edbb3fa
Jason Koth <jbkoth51@gmail.com>
2016-12-15 18:57:40 -0800

    Finished the program with Beautiful Soup to parse paragraph tags, doing better this week took a long time. But done though and moving on!

--- Ex12_4.py
+++ Ex12_4.py
@@ -6,11 +6,6 @@ html = urlopen(url).read()
 
 soup = BeautifulSoup(html, "html.parser")
 
-tags = soup('a')
-
-for tag in tags:
-
-	print('TAG:', tag)
-	print('URL:', tag.get('href', None))
-	print('Contents:', tag.contents[0])
-	print('Attrs:', tag.attrs)
+tags = soup("p")
+	
+print('The number of paragraphs on this page is', len(tags))
